6de94fedbb4c7f76a244f52ee4b76154a19f5d22
Rob Rowntree <rowntreerob@gmail.com>
2021-09-30 14:11:26 -0700

    modern pipe stmts for pipe thru api calls to browser

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   res.render('index', {});
 })
 
-app.get('/tweets', async function(req, resp, next) {
+app.get('/tweetsol', async function(req, resp, next) {
   const url = "https://api.twitter.com/2/users/1137701/tweets?end_time=2021-09-16T00:00:01Z&max_results=20";
   const opts = {method: 'GET', headers: {
     "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAADP4kAAAAAAAZFAIe7uhroCEeH6Dm6YllhQGup0%3D43FG0YrTlc6Q8Yn2CFGQO1dLv3dvXOGuLQgzGJqfY9HklYUmqn"} };
@@ -23,6 +23,20 @@ app.get('/tweets', async function(req, resp, next) {
   const json = await response.json();
   console.log(JSON.stringify(json));
   resp.send(JSON.stringify(json));
+})
+
+app.get('/tweets',  function(req, resp, next) {
+  const url = "https://api.twitter.com/2/users/1137701/tweets?end_time=2021-09-16T00:00:01Z&max_results=20";
+  const opts = {method: 'GET', headers: {
+    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAADP4kAAAAAAAZFAIe7uhroCEeH6Dm6YllhQGup0%3D43FG0YrTlc6Q8Yn2CFGQO1dLv3dvXOGuLQgzGJqfY9HklYUmqn"} };
+  fetch(url, opts)
+    .then(response => {
+      resp.writeHead(response.status, response.headers);
+      response.body.pipe(resp);
+    }).catch((error) => {
+        console.error(error);
+        next(error);
+      });
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
