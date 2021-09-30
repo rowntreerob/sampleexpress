35e79ae0cb4621088dbc4cf46f1b0ed2da7937eb
Rob Rowntree <rowntreerob@gmail.com>
2021-09-29 17:19:44 -0700

    route ./tweets added

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 /**
  * @author Léo Unbekandt
  */
-
-var express = require('express')
+import fetch from 'node-fetch';
+import express from 'express';
+import { URL } from 'url';
 var app = express()
 
+const __dirname = new URL('.', import.meta.url).pathname;
 app.use(express.static(__dirname + '/public'));
-
-
 
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   res.render('index', {});
+})
+
+app.get('/tweets', async function(req, resp, next) {
+  const url = "https://api.twitter.com/2/users/1137701/tweets?end_time=2021-09-16T00:00:01Z&max_results=20";
+  const opts = {method: 'GET', headers: {
+    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAADP4kAAAAAAAZFAIe7uhroCEeH6Dm6YllhQGup0%3D43FG0YrTlc6Q8Yn2CFGQO1dLv3dvXOGuLQgzGJqfY9HklYUmqn"} };
+  const response = await fetch(url, opts);
+  const json = await response.json();
+  console.log(JSON.stringify(json));
+  resp.send(JSON.stringify(json));
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
