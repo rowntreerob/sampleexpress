0cd31e0d937b933ba5b367e5c19fe7f91b74beb7
Rob Rowntree <rowntreerob@gmail.com>
2021-10-04 10:05:13 -0700

    rm the private credentials for public post of project

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
+## Bubble utility - binary file mover ( node express server )
+
+A feature to simply move around binary files, with Endpoints or with API Connector using 2, encoded Urls  (from , to) as parameters. For, example to move/copy a jpeg from AWS S3 platform to another, 3rd party fileSys as a service, the Url for the Endpoint/ connector would be :
+
+```
+http://localhost:3000/files/https%3A%2F%2Fs3.amazonaws.com%2Fyayatv-dev%2F3c6429451c4c3af2d8e18085f957e612_image.jpeg/https%3A%2F%2Fs3.amazonaws.com%2Fyayatv-dev%2F3c6429451c4c3af2d8e18085f957e612_image.jpeg
+```
+and the node express route corresponding to the above client request is :
+
+```
+app.get('/files/:from/:to',  function(req, resp, next) {
+  var url1, url2 = "";
+```
+
+Response is standard **http.response**
+
+Full stdout log from curl client moving file from AWS to Parse-Server on [pastBin](https://pastebin.com/uG4p7Qij)
+
+Intended for devs with some node.express and some npm and some git background.
+
+## Download, Install ##
+using std. git.clone and node.npm.install
+
+## dev run   localhost:3000 ##
+```
+cd $PROJECT_ROOT
+node app
+```
+
+use curl client , use Postman for client
+remember to urlencode  each of the 2 url parms located in the full url's path.
+
 # Sample Application with Node.js and Express Framework
 
 This sample is running on: https://node-express.is-easy-on-scalingo.com/
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * @author Léo Unbekandt
+ * @author Léo Unbekandt 
  */
 import fetch from 'node-fetch';
 import express from 'express';
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 app.get('/tweetsol', async function(req, resp, next) {
   const url = "https://api.twitter.com/2/users/1137701/tweets?end_time=2021-09-16T00:00:01Z&max_results=20";
   const opts = {method: 'GET', headers: {
-    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAADP4kAAAAAAAZFAIe7uhroCEeH6Dm6YllhQGup0%3D43FG0YrTlc6Q8Yn2CFGQO1dLv3dvXOGuLQgzGJqfY9HklYUmqn"} };
+    "Authorization": "$TOKEN"} };
   const response = await fetch(url, opts);
   const json = await response.json();
   console.log(JSON.stringify(json));
@@ -28,12 +28,42 @@ app.get('/tweetsol', async function(req, resp, next) {
 app.get('/tweets',  function(req, resp, next) {
   const url = "https://api.twitter.com/2/users/1137701/tweets?end_time=2021-09-16T00:00:01Z&max_results=20";
   const opts = {method: 'GET', headers: {
-    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAADP4kAAAAAAAZFAIe7uhroCEeH6Dm6YllhQGup0%3D43FG0YrTlc6Q8Yn2CFGQO1dLv3dvXOGuLQgzGJqfY9HklYUmqn"} };
+    "Authorization": "$TOKEN"} };
   fetch(url, opts)
     .then(response => {
       resp.writeHead(response.status, response.headers);
       response.body.pipe(resp);
     }).catch((error) => {
+        console.error(error);
+        next(error);
+      });
+})
+// url from and to.. fetch from url1 and POST to url2
+// return response.body from saas "sender"
+// use a pipe to flip that resp.body to the payload of the POST
+// to saas "receiver" returning the POST.response to orig client
+app.get('/files/:from/:to',  function(req, resp, next) {
+  var url1, url2 = "";
+  if (req.params.from)url1 = decodeURI(req.params.from);
+  if (req.params.to)url2 = decodeURI(req.params.to);
+  fetch(url1)
+    .then(response => {
+      return response.body;
+    }).then(body => {
+      var opts2 = {method: 'POST', headers: {
+        "X-Parse-Application-Id": "$ID",
+        "X-Parse-REST-API-Key": "$KEY",
+        "Content-Type": "	image/jpeg"
+        },
+        body: body,
+       };
+      fetch(url2, opts2 )
+        .then(respon2 => {
+          resp.writeHead(respon2.status, respon2.headers);
+          respon2.body.pipe(resp);
+      }) // Promise wrapper for POST action
+    })  // Promise wrapper of body of init GET
+      .catch((error) => {
         console.error(error);
         next(error);
       });
